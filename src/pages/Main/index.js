a10b41c9e30535409d800ecbd8eba251ca6186e3
JOSE <gccinane@gmail.com>
2020-04-23 09:54:00 -0300

    listagem de usuarios

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,20 +1,48 @@
 import React, { Component } from 'react';
+import { Keyboard, ActivityIndicator } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import { Container, SubmitButton, Form, Input } from './styles';
-
+import {
+  Container,
+  SubmitButton,
+  Form,
+  Input,
+  List,
+  User,
+  Avatar,
+  Name,
+  Bio,
+  ProfileButton,
+  ProfileButtonText,
+} from './styles';
+import api from '../services/api';
 export default class Main extends Component {
   state = {
     newUser: '',
     users: [],
   };
 
-  handleAddUser = () => {
-    console.log();
-    console.tron.log(this.state.newUser);
+  handleAddUser = async () => {
+    const { users, newUser } = this.state;
+    const res = await api.get(`/users/${newUser}`);
+
+    const data = {
+      name: res.data.name,
+      login: res.data.login,
+      bio: res.data.bio,
+      avatar: res.data.avatar_url,
+    };
+
+    this.setState({
+      users: [...users, data],
+      newUser: '',
+      loading: false,
+    });
+
+    Keyboard.dismiss();
   };
 
   render() {
-    const { newUser } = this.state;
+    const { newUser, users } = this.state;
     return (
       <Container>
         <Form>
@@ -31,6 +59,20 @@ export default class Main extends Component {
             <Icon name="add" size={20} color="#fff" />
           </SubmitButton>
         </Form>
+        <List
+          data={users}
+          keyExtractor={(user) => user.login}
+          renderItem={({ item }) => (
+            <User>
+              <Avatar source={{ uri: item.avatar }} />
+              <Name>{item.name}</Name>
+              <Bio>{item.bio}</Bio>
+              <ProfileButton onPress={() => { }}>
+                <ProfileButtonText>Ver perfil</ProfileButtonText>
+              </ProfileButton>
+            </User>
+          )}
+        />
       </Container>
     );
   }
